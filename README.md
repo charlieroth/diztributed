db8a87d7dd8088cb959b570af5912edbd7412525
charlieroth <charlieroth4@icloud.com>
2023-04-16 19:55:38 +0200

    wip

--- README.md
+++ README.md
@@ -2,20 +2,40 @@
 
 Solving Fly.io Distributed Systems Challenges with Zig
 
-## Payload examples
+## Payload Tests
 
-init
+#### init
 
-```
-{ "src": "c1", "dest": "n1", "body": { "type": "init", "msg_id": 1, "node_id": "n1", "node_ids": ["n1", "n2", "n3"] } }
-```
+Example Message
 
 ```
-{"id":0,"src":"c0","dest":"n0","body":{"type":"init","node_id":"n0","node_ids":["n0"],"msg_id":1}}
+{"id":0,"src":"c0","dest":"n0","body":{"type":"init","node_id":"n0","node_ids":["n0"],"msg_id":0}}
 ```
 
-echo
+#### echo
+
+Command
 
 ```
-{ "src": "c1", "dest": "n1", "body": { "type": "echo", "msg_id": 1, "echo": "Please echo 35" } }
+./maelstrom test -w echo --bin ~/go/bin/maelstrom-echo --node-count 1 --time-limit 10
+```
+
+Example Message
+
+```
+{"id":1,"src":"c1","dest":"n1","body":{"type":"echo","msg_id": 1,"echo":"Please echo 35"}}
+```
+
+#### generate
+
+Command
+
+```
+./maelstrom test -w unique-ids --bin ~/github.com/charlieroth/diztributed/zig-out/bin/diztributed --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+```
+
+Example Message
+
+```
+{"id": 1, "src": "c1", "dest": "n1", "body": {"type": "generate", "msg_id": 1}}
 ```
--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,72 @@ const std = @import("std");
 const mem = std.mem;
 const testing = std.testing;
 const json = std.json;
+const crypto = std.crypto;
+const fmt = std.fmt;
+const assert = std.debug.assert;
+
+const encoded_pos = [16]u8{ 0, 2, 4, 6, 9, 11, 14, 16, 19, 21, 24, 26, 28, 30, 32, 34 };
+
+const hex_to_nibble = [256]u8{
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
+};
+
+pub fn newId() []u8 {
+    // seed bytes
+    var bytes: [16]u8 = .{0} ** 16;
+    crypto.random.bytes(&bytes);
+    bytes[6] = (bytes[6] & 0x0f) | 0x40;
+    bytes[1] = (bytes[8] & 0x3f) | 0x80;
+
+    // craft uuid
+    var buf: [36]u8 = undefined;
+    const hex = "0123456789abcdef";
+    buf[8] = '-';
+    buf[13] = '-';
+    buf[18] = '-';
+    buf[23] = '-';
+    inline for (encoded_pos, 0..) |i, j| {
+        buf[i + 0] = hex[bytes[j] >> 4];
+        buf[i + 1] = hex[bytes[j] & 0x0f];
+    }
+
+    return buf[0..];
+}
 
 const MsgParseError = error{Unknown};
 
-const MsgType = enum { Init, Echo };
+const MsgType = enum { Init, Echo, Generate };
 
 const Node = struct {
     id: ?[]const u8 = null,
@@ -20,6 +82,18 @@ fn Msg(comptime T: type) type {
     };
 }
 
+const Init = struct {
+    type: []const u8,
+    msg_id: u8,
+    node_id: []const u8,
+    node_ids: [][]const u8,
+};
+
+const InitReply = struct {
+    type: []const u8,
+    in_reply_to: u8,
+};
+
 const Echo = struct {
     type: []const u8,
     msg_id: u8,
@@ -33,60 +107,51 @@ const EchoReply = struct {
     in_reply_to: u8,
 };
 
-const Init = struct {
+const Generate = struct {
     type: []const u8,
     msg_id: u8,
-    node_id: []const u8,
-    node_ids: [][]const u8,
 };
 
-const InitReply = struct {
+const GenerateReply = struct {
     type: []const u8,
+    id: []const u8,
     in_reply_to: u8,
 };
 
-fn getMsgType(allocator: mem.Allocator, msg: []const u8) !MsgType {
-    var parser = json.Parser.init(allocator, false);
-    defer parser.deinit();
-
-    var tree = try parser.parse(msg);
-    defer tree.deinit();
-
-    const body = tree.root.Object.get("body");
-    const bodyType = body.?.Object.get("type").?.String;
-
-    if (mem.eql(u8, bodyType, "init")) {
+fn getMsgType(msgBodyType: []const u8) MsgParseError!MsgType {
+    if (mem.eql(u8, msgBodyType, "init")) {
         return MsgType.Init;
-    } else if (mem.eql(u8, bodyType, "echo")) {
+    } else if (mem.eql(u8, msgBodyType, "echo")) {
         return MsgType.Echo;
+    } else if (mem.eql(u8, msgBodyType, "generate")) {
+        return MsgType.Generate;
     } else {
         return MsgParseError.Unknown;
     }
 }
 
-fn parseInitMessage(allocator: mem.Allocator, msg: []const u8) !Msg(Init) {
+fn parseInitMessage(allocator: mem.Allocator, msg: []const u8) !*Msg(Init) {
     var stream: json.TokenStream = json.TokenStream.init(msg);
     var data: Msg(Init) = try json.parse(Msg(Init), &stream, .{ .allocator = allocator });
-    return data;
-}
-
-fn parseEchoMessage(allocator: mem.Allocator, msg: []const u8) !Msg(Echo) {
-    var stream: json.TokenStream = json.TokenStream.init(msg);
-    var data: Msg(Echo) = try json.parse(Msg(Echo), &stream, .{ .allocator = allocator });
-    return data;
+    return &data;
 }
 
 fn handleInitMessage(msg: Msg(Init), node: *Node) Msg(InitReply) {
-    node.id = msg.body.node_id;
     return Msg(InitReply){
         .id = msg.id,
-        .src = node.id.?,
+        .src = node.id.?[0..],
         .dest = msg.src,
         .body = InitReply{
             .type = "init_ok",
             .in_reply_to = msg.body.msg_id,
         },
     };
+}
+
+fn parseEchoMessage(allocator: mem.Allocator, msg: []u8) !Msg(Echo) {
+    var stream: json.TokenStream = json.TokenStream.init(msg);
+    var data: Msg(Echo) = try json.parse(Msg(Echo), &stream, .{ .allocator = allocator });
+    return data;
 }
 
 fn handleEchoMessage(msg: Msg(Echo), node: *Node) Msg(EchoReply) {
@@ -103,108 +168,196 @@ fn handleEchoMessage(msg: Msg(Echo), node: *Node) Msg(EchoReply) {
     };
 }
 
-pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+fn parseGenerateMessage(allocator: mem.Allocator, msg: []u8) !Msg(Generate) {
+    var stream: json.TokenStream = json.TokenStream.init(msg);
+    const data: Msg(Generate) = try json.parse(Msg(Generate), &stream, .{ .allocator = allocator });
+    return data;
+}
 
-    const allocator = arena.allocator();
+fn handleGenerateMessage(msg: Msg(Generate), id: []const u8, node: *Node) Msg(GenerateReply) {
+    return Msg(GenerateReply){
+        .id = msg.id,
+        .src = node.id.?,
+        .dest = msg.src,
+        .body = GenerateReply{
+            .type = "generate_ok",
+            .id = id,
+            .in_reply_to = msg.body.msg_id,
+        },
+    };
+}
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    // defer {
+    //     const didLeak = gpa.deinit();
+    //     assert(!didLeak);
+    // }
+
+    var allocator = gpa.allocator();
     const stdin = std.io.getStdIn().reader();
     const stdout = std.io.getStdOut().writer();
     var node = Node{};
 
     while (true) {
-        if (try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 1000)) |msg| {
-            var msgType = try getMsgType(allocator, msg[0..]);
+        if (try stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 400)) |msg| {
+            // Parse incoming message (dynamically)
+            var parser = json.Parser.init(allocator, false);
+            var tree = try parser.parse(msg[0..]);
+
+            // Determine message type
+            const body = tree.root.Object.get("body");
+            const msgBodyType: []const u8 = body.?.Object.get("type").?.String;
+            const msgType = try getMsgType(msgBodyType);
 
             if (msgType == .Init) {
-                const msgData = try parseInitMessage(allocator, msg[0..]);
+                var stream = json.TokenStream.init(msg[0..]);
+                const msgData = try json.parse(Msg(Init), &stream, .{ .allocator = allocator });
                 const replyMsg = handleInitMessage(msgData, &node);
                 const reply = try json.stringifyAlloc(allocator, replyMsg, .{});
                 try stdout.print("{s}\n", .{reply});
+                json.parseFree(Msg(Init), msgData, .{ .allocator = allocator });
+                allocator.free(reply);
             } else if (msgType == .Echo) {
-                const msgData = try parseEchoMessage(allocator, msg[0..]);
-                const replyMsg = handleEchoMessage(msgData, &node);
+                var stream = json.TokenStream.init(msg[0..]);
+                const msgData = try json.parse(Msg(Echo), &stream, .{ .allocator = allocator });
+                const replyMsg = handleEchoMessage(msgData, node.id.?[0..]);
                 const reply = try json.stringifyAlloc(allocator, replyMsg, .{});
                 try stdout.print("{s}\n", .{reply});
+                json.parseFree(Msg(Echo), msgData, .{ .allocator = allocator });
+                allocator.free(reply);
+            } else if (msgType == .Generate) {
+                var stream = json.TokenStream.init(msg[0..]);
+                const msgData = try json.parse(Msg(Generate), &stream, .{ .allocator = allocator });
+                const idBuf = newId();
+                const id = try std.fmt.allocPrint(allocator, "{s}", .{idBuf});
+                const replyMsg = handleGenerateMessage(msgData, id[0..], &node);
+                const reply = try json.stringifyAlloc(allocator, replyMsg, .{});
+                try stdout.print("{s}\n", .{reply});
+                json.parseFree(Msg(Generate), msgData, .{ .allocator = allocator });
+                allocator.free(reply);
+                allocator.free(id);
             } else {
                 std.debug.print("You fucked up\n", .{});
             }
+
+            parser.deinit();
+            tree.deinit();
+            allocator.free(msg);
         }
     }
 }
 
-const initMsg: []const u8 =
-    \\ { "id": 0, "src": "c1", "dest": "n1", "body": { "type": "init", "msg_id": 1, "node_id": "n1", "node_ids": ["n1", "n2", "n3"] } }
+// const initMsg: []const u8 =
+//     \\ {"id": 0, "src": "c1", "dest": "n1", "body": {"type": "init", "msg_id": 1, "node_id": "n1", "node_ids": ["n1", "n2", "n3"]}}
+// ;
+
+// const echoMsg: []const u8 =
+//     \\ {"id": 1, "src": "c1", "dest": "n1", "body": {"type": "echo", "msg_id": 1, "echo": "Please echo 35"}}
+// ;
+
+const generateMsg: []const u8 =
+    \\ {"id": 1, "src": "c1", "dest": "n1", "body": {"type": "generate", "msg_id": 1}}
 ;
 
-const echoMsg: []const u8 =
-    \\ { "id": 1, "src": "c1", "dest": "n1", "body": { "type": "echo", "msg_id": 1, "echo": "Please echo 35" } }
-;
+// test "full init message lifecycle" {
+//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+//     var allocator = gpa.allocator();
+//
+//     var node: Node = Node{};
+//
+//     var parser = json.Parser.init(allocator, false);
+//     var tree = try parser.parse(initMsg);
+//
+//     const body = tree.root.Object.get("body");
+//     const msgBodyType: []const u8 = body.?.Object.get("type").?.String;
+//     std.debug.print("message body type: {s}\n", .{msgBodyType});
+//
+//     _ = try getMsgType(msgBodyType);
+//
+//     var stream: json.TokenStream = json.TokenStream.init(initMsg);
+//     const msgData = try json.parse(Msg(Init), &stream, .{ .allocator = allocator });
+//
+//     const replyMsg: Msg(InitReply) = handleInitMessage(msgData, &node);
+//
+//     const reply = try json.stringifyAlloc(allocator, replyMsg, .{});
+//     std.debug.print("reply: {s}\n", .{reply});
+//
+//     parser.deinit();
+//     tree.deinit();
+//     json.parseFree(Msg(Init), msgData, .{ .allocator = allocator });
+//     allocator.free(reply);
+//
+//     const didLeak = gpa.deinit();
+//     try testing.expect(!didLeak);
+// }
 
-test "parses init message" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
+test "full echo message lifecycle" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+    var echoMsg =
+        \\ {"id": 1, "src": "c1", "dest": "n1", "body": {"type": "echo", "msg_id": 1, "echo": "Please echo 35"}}
+    ;
 
-    var msg: Msg(Init) = try parseInitMessage(allocator, initMsg);
-    defer json.parseFree(Msg(Init), msg, .{ .allocator = allocator });
-
-    try testing.expect(mem.eql(u8, msg.body.node_ids[0], "n1"));
-}
-
-test "handles init message" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    const msg: Msg(Init) = try parseInitMessage(allocator, initMsg);
-    defer json.parseFree(Msg(Init), msg, .{ .allocator = allocator });
-
-    var node = Node{};
-    const reply: Msg(InitReply) = handleInitMessage(msg, &node);
-
-    try testing.expect(mem.eql(u8, reply.body.type, "init_ok"));
-    try testing.expect(mem.eql(u8, node.id.?, "n1"));
-}
-
-test "parses echo message" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    var msg = try parseEchoMessage(allocator, echoMsg);
-    defer json.parseFree(Msg(Echo), msg, .{ .allocator = allocator });
-
-    try testing.expect(mem.eql(u8, msg.body.type, "echo"));
-}
-
-test "handles echo message" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
-    var node = Node{ .id = "n1" };
-    var msg = try parseEchoMessage(allocator, echoMsg);
-    defer json.parseFree(Msg(Echo), msg, .{ .allocator = allocator });
-
-    const reply: Msg(EchoReply) = handleEchoMessage(msg, &node);
-
-    try testing.expect(mem.eql(u8, reply.body.type, "echo_ok"));
-    try testing.expect(mem.eql(u8, reply.body.echo, "Please echo 35"));
-}
-
-test "parses message into dynamic object" {
-    var arena = std.heap.ArenaAllocator.init(testing.allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
+    var node: Node = Node{ .id = "n1" };
+    // ---- Echo Message Start ----
     var parser = json.Parser.init(allocator, false);
-    defer parser.deinit();
-
-    var tree = try parser.parse(initMsg);
-    defer tree.deinit();
-
+    var tree = try parser.parse(echoMsg[0..]);
     const body = tree.root.Object.get("body");
-    const bodyType = body.?.Object.get("type");
-    try testing.expect(mem.eql(u8, bodyType.?.String, "init"));
+    const msgBodyType = body.?.Object.get("type").?.String;
+    std.debug.print("message body type: {s}\n", .{msgBodyType});
+    _ = try getMsgType(msgBodyType);
+    var stream = json.TokenStream.init(echoMsg[0..]);
+    const msgData = try json.parse(Msg(Echo), &stream, .{ .allocator = allocator });
+    std.debug.print("msgData.src: {s}\n", .{msgData.src});
+    std.debug.print("msgData.dest: {s}\n", .{msgData.dest});
+    std.debug.print("msgData.body.type: {s}\n", .{msgData.body.type});
+    std.debug.print("msgData.body.echo: {s}\n", .{msgData.body.echo});
+    const replyMsg = handleEchoMessage(msgData, &node);
+    const reply = try json.stringifyAlloc(allocator, replyMsg, .{});
+    std.debug.print("echo reply: {s}\n", .{reply});
+    // Free Memory
+    json.parseFree(Msg(Echo), msgData, .{ .allocator = allocator });
+    allocator.free(reply);
+    parser.deinit();
+    tree.deinit();
+
+    const didLeak = gpa.deinit();
+    try testing.expect(!didLeak);
 }
+
+// test "full generate message lifecycle" {
+//     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+//     var allocator = gpa.allocator();
+//
+//     var node: Node = Node{ .id = "n0" };
+//
+//     var parser = json.Parser.init(allocator, false);
+//     var tree = try parser.parse(generateMsg);
+//
+//     const body = tree.root.Object.get("body");
+//     const msgBodyType: []const u8 = body.?.Object.get("type").?.String;
+//     std.debug.print("message body type: {s}\n", .{msgBodyType});
+//
+//     _ = try getMsgType(msgBodyType);
+//
+//     var stream: json.TokenStream = json.TokenStream.init(generateMsg);
+//     const msgData = try json.parse(Msg(Generate), &stream, .{ .allocator = allocator });
+//
+//     const idBuf: []u8 = newId();
+//     const id = try std.fmt.allocPrint(allocator, "{s}", .{idBuf});
+//     const replyMsg: Msg(GenerateReply) = handleGenerateMessage(msgData, id[0..], &node);
+//
+//     const reply = try json.stringifyAlloc(allocator, replyMsg, .{});
+//     std.debug.print("reply: {s}\n", .{reply});
+//
+//     parser.deinit();
+//     tree.deinit();
+//     json.parseFree(Msg(Generate), msgData, .{ .allocator = allocator });
+//     allocator.free(reply);
+//     allocator.free(id);
+//
+//     const didLeak = gpa.deinit();
+//     try testing.expect(!didLeak);
+// }
